e5c470cebc00785f59946663308dd2c47ba7e8a4
sassan <s@s.com>
2025-05-15 18:53:55 +0330

    feat: add custom user model and user manager

--- core/users/models.py
+++ core/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from core.common.models import TimeStampedModel
+from core.users.managers import UserManager
+
+
+class User(AbstractUser, TimeStampedModel):
+    """Custom User model."""
+
+    email = models.EmailField(_("email address"), unique=True)
+    name = models.CharField(_("full name"), max_length=150, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self) -> str:
+        return self.email
